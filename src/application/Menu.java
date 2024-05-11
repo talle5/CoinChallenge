40753fa3c3e79168e2943879494c9bb77790685a
talle5 <talles.lima@alu.ufc.br>
2024-05-11 20:44:29 -0300

    corrigir ramificaçoes

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -19,40 +19,50 @@ public class Menu implements IConsoleApplication {
     public void run() {
         var consoleInput = new Scanner(System.in);
         var swapper = new Swapper();
-        System.out.println(WELCOME_MESSAGE);
-        var choice = consoleInput.nextInt();
-        if (choice == 7 ) {
-            (new Shell()).run();
-            return;
-        }
-        System.out.println("Digite o valor que deseja comverter:");
-        var valor = consoleInput.nextDouble();
-        switch (choice) {
-            case 1 -> {
-                var resultado = swapper.swap("BRL", "USD", valor);
-                System.out.printf("%2.2f Reais valem %2.2f Dolares\n", valor, resultado);
+        while (true) {
+            System.out.println(WELCOME_MESSAGE);
+            var choice = consoleInput.nextInt();
+            if (choice > 0 && choice < 7) {
+                System.out.println("Digite o valor que deseja comverter:");
+                if (consoleInput.hasNextDouble()) {
+                    var valor = consoleInput.nextDouble();
+                    switch (choice) {
+                        case 1 -> {
+                            var resultado = swapper.swap("BRL", "USD", valor);
+                            System.out.printf("%2.2f Reais valem %2.2f Dolares\n", valor, resultado);
+                        }
+                        case 2 -> {
+                            var resultado = swapper.swap("BRL", "BOB", valor);
+                            System.out.printf("%2.2f Reais valem %2.2f Pesos Bolivianos\n", valor, resultado);
+                        }
+                        case 3 -> {
+                            var resultado = swapper.swap("BRL", "ARS", valor);
+                            System.out.printf("%2.2f Reais valem %2.2f Pesos Argentinos\n", valor, resultado);
+                        }
+                        case 4 -> {
+                            var resultado = swapper.swap("USD", "BRL", valor);
+                            System.out.printf("%2.2f Reais valem %2.2f Reais\n", valor, resultado);
+                        }
+                        case 5 -> {
+                            var resultado = swapper.swap("BOB", "BRL", valor);
+                            System.out.printf("%2.2f Pesos Bolivianos valem %2.2f Reais\n", valor, resultado);
+                        }
+                        case 6 -> {
+                            var resultado = swapper.swap("ARS", "BRL", valor);
+                            System.out.printf("%2.2f Pesos Argentinos valem %2.2f Reais\n", valor, resultado);
+                        }
+                    }
+                } else {
+                    System.out.println("Digite um numero!");
+                }
+            } else if (choice == 7) {
+                (new Shell()).run();
+                return;
+            } else if (choice == 8) {
+                break;
+            } else {
+                System.out.println("Digite uma opção valida!");
             }
-            case 2 -> {
-                var resultado = swapper.swap("BRL", "BOB", valor);
-                System.out.printf("%2.2f Reais valem %2.2f Pesos Bolivianos\n", valor, resultado);
-            }
-            case 3 -> {
-                var resultado = swapper.swap("BRL", "ARS", valor);
-                System.out.printf("%2.2f Reais valem %2.2f Pesos Argentinos\n", valor, resultado);
-            }
-            case 4 -> {
-                var resultado = swapper.swap("USD", "BRL", valor);
-                System.out.printf("%2.2f Reais valem %2.2f Reais\n", valor, resultado);
-            }
-            case 5 -> {
-                var resultado = swapper.swap("BOB", "BRL", valor);
-                System.out.printf("%2.2f Pesos Bolivianos valem %2.2f Reais\n", valor, resultado);
-            }
-            case 6 -> {
-                var resultado = swapper.swap("ARS", "BRL", valor);
-                System.out.printf("%2.2f Pesos Argentinos valem %2.2f Reais\n", valor, resultado);
-            }
-            default -> System.out.println("Digite uma opção valida!");
         }
         consoleInput.close();
     }
